80128259fc3cfd6c7ee14f06ac2926cd7e8f3630
jdaguilar <jorgeaguilar2761@gmail.com>
2024-08-29 14:47:09 -0500

    feat: create clear bash script

--- clear.sh
+++ clear.sh
@@ -1,1 +1,10 @@
 sudo kubectl delete namespace spark
+
+sudo microk8s kubectl-minio delete
+sudo microk8s disable rbac
+sudo microk8s disable storage
+sudo microk8s disable storage
+sudo microk8s disable hostpath-storage
+sudo microk8s disable metallb
+
+sudo snap remove microk8s --purge
