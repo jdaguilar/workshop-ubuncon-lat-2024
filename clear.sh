ca6ebf1dde0421878810821242bc1b24d4dbdb90
jdaguilar <jorgeaguilar2761@gmail.com>
2024-08-30 01:30:02 -0500

    feat: clear microk8s deployment

--- clear.sh
+++ clear.sh
@@ -1,10 +1,10 @@
 sudo kubectl delete namespace spark
 
-sudo microk8s kubectl-minio delete
-sudo microk8s disable rbac
-sudo microk8s disable storage
-sudo microk8s disable storage
-sudo microk8s disable hostpath-storage
-sudo microk8s disable metallb
+sudo microk8s kubectl-minio delete -y
+sudo microk8s disable rbac -y
+sudo microk8s disable storage -y
+sudo microk8s disable storage -y
+sudo microk8s disable hostpath-storage -y
+sudo microk8s disable metallb -y
 
 sudo snap remove microk8s --purge
