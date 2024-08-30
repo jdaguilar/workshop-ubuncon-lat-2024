807cba9a7b80870d23e6add701b4e03a22dd7b3a
jdaguilar <jorgeaguilar2761@gmail.com>
2024-08-30 04:48:04 -0500

    create script to upload existing data

---
+++ upload_data.sh
@@ -0,0 +1,26 @@
+#!/usr/bin/env bash
+
+set -euo pipefail
+
+# Global variable
+export MINIO_PROFILE_NAME="minio-local"
+
+# Function definitions
+print_info() { echo -e "\e[32m* $1\e[0m"; }
+print_warn() { echo -e "\e[33m* WARNING: $1\e[0m"; }
+print_error() {
+    echo -e "\e[31m* ERROR: $1\e[0m"
+    exit 1
+}
+
+upload_initial_data() {
+    print_info "Uploading initial data to artifacts bucket..."
+    aws s3 cp minioserver/data s3://raw --recursive --profile $MINIO_PROFILE_NAME
+}
+
+# Main execution
+main() {
+    upload_initial_data
+}
+
+main "$@"
